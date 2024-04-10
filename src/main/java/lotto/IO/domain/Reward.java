d6e08cf4b99355bf80ea3822dcbeaa0694670f3d
2jin2.1031 <2jin2.1031@gmail.com>
2024-04-10 17:36:46 +0900

    Refactor: 변수명 컨벤션에 맞춰 수정

--- src/main/java/lotto/IO/domain/Reward.java
+++ src/main/java/lotto/IO/domain/Reward.java
@@ -4,10 +4,10 @@ public class Reward {
 
     private final int reward;
 
-    public Reward(int[] WinPrice, int[] correctCounts) {
+    public Reward(int[] winPrice, int[] correctCounts) {
         int sum = 0;
-        for (int i = 0; i < WinPrice.length; i++) {
-            sum += WinPrice[i] * correctCounts[i];
+        for (int i = 0; i < winPrice.length; i++) {
+            sum += winPrice[i] * correctCounts[i];
         }
         this.reward = sum;
     }
