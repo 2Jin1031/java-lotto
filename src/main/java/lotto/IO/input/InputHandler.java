103c69e2b2a95f46ce1bc7e1dd119fbd14cd18f4
2jin2.1031 <2jin2.1031@gmail.com>
2024-04-11 00:25:29 +0900

    Refactor: 메서드 분리 및 컬렉션 변경 in InputHandler

--- src/main/java/lotto/IO/input/InputHandler.java
+++ src/main/java/lotto/IO/input/InputHandler.java
@@ -5,45 +5,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputHandler {
-    
+
     public static Integer getUserInputAmount() {
         String input = Console.readLine();
-        int amount;
+        return checkNumberFormat(input);
+    }
+
+    public static List<Integer> getUserInputLotto() {
+        String input = Console.readLine();
+
+        List<String> inputs = List.of(input.split(","));
+        List<Integer> lottoNums = new ArrayList<>();
+
+        inputs.forEach(element -> {
+            checkNumbersFormat(element, lottoNums);
+        });
+        return lottoNums;
+    }
+
+    public static Integer getUserInputBonus() {
+        String input = Console.readLine();
+        return checkNumberFormat(input);
+    }
+
+    private static void checkNumbersFormat(String element, List<Integer> lottoNums) {
+        try {
+            int lotto = Integer.parseInt(element.trim());
+            lottoNums.add(lotto);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
+        }
+    }
+
+    private static int checkNumberFormat(String input) {
+        int amount = 0;
         try {
             amount = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
         }
         return amount;
-    }
-
-    public static List<Integer> getUserInputLotto() {
-
-        String input = Console.readLine();
-
-        String[] inputs = input.split(",");
-        List<Integer> lottoNums = new ArrayList<>();
-
-        for (String element : inputs) {
-            try {
-                int lotto = Integer.parseInt(element.trim());
-                lottoNums.add(lotto);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
-            }
-        }
-        return lottoNums;
-    }
-
-    public static Integer getUserInputBonus() {
-
-        String input = Console.readLine();
-        int bonus;
-        try {
-            bonus = Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("올바른 숫자 형식이 아닙니다.");
-        }
-        return bonus;
     }
 }
